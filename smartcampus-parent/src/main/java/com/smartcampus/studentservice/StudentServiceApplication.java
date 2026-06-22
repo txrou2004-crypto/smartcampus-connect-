7b8fc6628b89e25f9b820adeb4edee27dddb1b8e
jiaxuan-2124 <jiaxuanling2004@gmail.com>
2026-06-22 18:32:45 +0800

    Refactor StudentServiceApplication to use Spring Boot

--- smartcampus-parent/src/main/java/com/smartcampus/studentservice/StudentServiceApplication.java
+++ smartcampus-parent/src/main/java/com/smartcampus/studentservice/StudentServiceApplication.java
@@ -1,10 +1,15 @@
 package com.smartcampus.studentservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
 public class StudentServiceApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SpringApplication.run(StudentServiceApplication.class, args)
 	}
 
 }
