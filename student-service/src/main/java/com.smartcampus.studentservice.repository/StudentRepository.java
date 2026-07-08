57821f3ea6996e393b2678f19fcda27a4a46de9f
nornadhirah03 <nornadhirah03@gmail.com>
2026-07-08 23:21:36 +0800

    Create StudentRepository.java

---
+++ student-service/src/main/java/com.smartcampus.studentservice.repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.smartcampus.studentservice.repository;
+
+import com.smartcampus.studentservice.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+}
