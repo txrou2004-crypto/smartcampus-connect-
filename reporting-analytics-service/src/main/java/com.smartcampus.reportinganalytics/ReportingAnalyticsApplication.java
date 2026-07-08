faf4d44358373996f087a5cbbc1db0792811607c
nornadhirah03 <nornadhirah03@gmail.com>
2026-07-08 23:04:41 +0800

    Create ReportingAnalyticsApplication.java

---
+++ reporting-analytics-service/src/main/java/com.smartcampus.reportinganalytics/ReportingAnalyticsApplication.java
@@ -0,0 +1,11 @@
+package com.smartcampus.reportinganalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingAnalyticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReportingAnalyticsApplication.class, args);
+    }
+}
